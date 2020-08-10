c7f0a7345e0f401dd2c1e5eb243990e40c150e5a
Sara Remi Fields <sfields@andrew.cmu.edu>
2020-08-10 16:29:52 -0400

    Sideways Movement

--- shapes.js
+++ shapes.js
@@ -5,13 +5,26 @@ $(document).ready(function(){
     animateDiv('.d');
 });
 
+// function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    // var h = $(window).height();
+    // var w = $(window).width();
+    
+    // var nh = Math.floor(Math.random() * h-200);
+    // var nw = Math.floor(Math.random() * w-200);
+    
+    //return [nh,nw];    
+    
+// }
+
 function makeNewPosition(){
     
     // Get viewport dimensions (remove the dimension of the div)
-    var h = $(window).height();
+    var h = 10;
     var w = $(window).width();
     
-    var nh = Math.floor(Math.random() * h-200);
+    var nh = 0;
     var nw = Math.floor(Math.random() * w-200);
     
     return [nh,nw];    
@@ -20,8 +33,14 @@ function makeNewPosition(){
 
 function animateDiv(myclass){
     var newq = makeNewPosition();
-    $(myclass).animate({ top: newq[0], left: newq[1] }, 20000,   function(){
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 9000,   function(){
       animateDiv(myclass);        
     });
+
+//function animateDiv(myclass){
+    //var newq = makeNewPosition();
+    //$(myclass).animate({ top: newq[0], left: newq[1] }, "slow",   function(){
+    //  animateDiv(myclass);        
+    //});
     
-};
+}; 
