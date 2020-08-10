32e169844dd506d69aaffa79809dd96d526f191b
Sara Remi Fields <sfields@andrew.cmu.edu>
2020-08-10 14:55:27 -0400

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -20,7 +20,7 @@ function makeNewPosition(){
 
 function animateDiv(myclass){
     var newq = makeNewPosition();
-    $(myclass).animate({ top: newq[0], left: newq[1] }, 1000,   function(){
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 100,   function(){
       animateDiv(myclass);        
     });
     
